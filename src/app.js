b2e655e7c0bb7422d885129aa0fc5e0ef84dd37e
unknown <battingking03@gmail.com>
2021-12-04 11:54:57 +0500

    adding new port path

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const path=require('path');
 const hbs=require('hbs');
 const app=express();
-const port=8000;
+const port=process.env.Port ||8000;
 
 const statpath=path.join(__dirname,("../public"));
 
@@ -29,5 +29,3 @@ app.get("*",(req,res)=>{
 app.listen(port,()=>{
     console.log(`listening to port at ${port}`);
 })
-
-// time 1:23:00
